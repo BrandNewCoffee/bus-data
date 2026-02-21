c31d9f3d9aa850c51929479918db50e8c271886d
YUCHI <ricky971120@gmail.com>
2026-02-22 01:55:15 +0800

    1.data neidebiaoqianmingchengkaitoubiandaxie2.yinweifaxian shapedata.py limiande SubRouteNameZh_tw qucheng RouteNameZh_tw ,daozhi shape ziliaomeiyoubanfabianrenfuxian,suoyihoulaiba shapedata.py de SUbRoute gaile,dan stopsdata.py meiyougai,dengdaochuwentizaishuo3. shapedta.py gen stopsdata.py xinzeng response.status_code!=200 deshihoushuchu None bingqie print cuowuzhiling,ranhouruguocuowu(tongchangshi API_rate_limit_exceeded )rangta continue xiaqu,buyaozhijieduandiao.YISHANG

--- stopsdata.py
+++ stopsdata.py
@@ -1,11 +1,11 @@
 import json
 import requests
 import time
-access_token="eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJER2lKNFE5bFg4WldFajlNNEE2amFVNm9JOGJVQ3RYWGV6OFdZVzh3ZkhrIn0.eyJleHAiOjE3NzE2MjEzMTIsImlhdCI6MTc3MTUzNDkxMiwianRpIjoiZmYxNGMwNDctNWJhZC00OTU5LTgzMDAtNjlmNDQwYWY4MDM3IiwiaXNzIjoiaHR0cHM6Ly90ZHgudHJhbnNwb3J0ZGF0YS50dy9hdXRoL3JlYWxtcy9URFhDb25uZWN0Iiwic3ViIjoiNDQzOTdjOWEtZGFkNC00MjNkLTk0YzAtNWI5ODllN2FkZDZkIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiMzEwNzU4LWYwYjJlMTA2LWEyNzUtNGU2NiIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsic3RhdGlzdGljIiwicHJlbWl1bSIsInBhcmtpbmdGZWUiLCJtYWFzIiwiYWR2YW5jZWQiLCJnZW9pbmZvIiwidmFsaWRhdG9yIiwidG91cmlzbSIsImhpc3RvcmljYWwiLCJjd2EiLCJiYXNpYyJdfSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwidXNlciI6ImY1ZWUzNjE5In0.RhVfW8gNaYWAeGybJF7gkrhMZecDx6zLU34Sl78Ib8v6mpUaI24IIU0RP3V1CU-JUn8WkYtvnpxQ_maVp2Vxhf8IQ6UKRnrW8A3orSXAIqhwVnG5Om030wUbb3vnFtSBKzKvYpXKsJ60IZ-W_gWb0fCjIDpx7i3Xp7aKJblXjQQ0AdGK9zX7xtl3uE0hsw2kMTG-wN2BwzC7QEBrETL4UtGwwO7InfYs2SMtKUMfWmCoGROryua94xLACTvEroPGRQcaXDlwRfc9LwqLonAn-YGbIB_STLa-MONXBoHDTpyjNvLZ1G545RV6nzTOM69YAsUxyCB1m1UcYPEiB74R-w"
+access_token="eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJER2lKNFE5bFg4WldFajlNNEE2amFVNm9JOGJVQ3RYWGV6OFdZVzh3ZkhrIn0.eyJleHAiOjE3NzE3MTgzMDAsImlhdCI6MTc3MTYzMTkwMCwianRpIjoiYWI3OTMwMzEtOWFhZS00ZTUxLWEyYjYtNWU3YTljNGVkNmU3IiwiaXNzIjoiaHR0cHM6Ly90ZHgudHJhbnNwb3J0ZGF0YS50dy9hdXRoL3JlYWxtcy9URFhDb25uZWN0Iiwic3ViIjoiNDQzOTdjOWEtZGFkNC00MjNkLTk0YzAtNWI5ODllN2FkZDZkIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiMzEwNzU4LWYwYjJlMTA2LWEyNzUtNGU2NiIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsic3RhdGlzdGljIiwicHJlbWl1bSIsInBhcmtpbmdGZWUiLCJtYWFzIiwiYWR2YW5jZWQiLCJnZW9pbmZvIiwidmFsaWRhdG9yIiwidG91cmlzbSIsImhpc3RvcmljYWwiLCJjd2EiLCJiYXNpYyJdfSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwidXNlciI6ImY1ZWUzNjE5In0.pBgESUUg-6fCctTmTVTHShjRhKXxUm1nBha8zRuicz3y2m5mW4o12d04GJ1uY5kRiQ3IZjJxdnaXgHwCa3lDOpVwF7hQ0yAwlUTps7TR9CYLLE4VOsA5k6IKAs3JfGTsushtqyiiK-Kb-yZ9dbWlcQYt9xab2pOXSvvNAjOCDAN_7Gp8jp4TetwlAvykiZ8gvCDtrpPSm-Zo6t98javlSRzWC7TybT-UvsGa12lDp4L2szmxR3B1ZCCE6_YuCs6QBUIMv0A5b1BHQr9MSWalYbsBbK3QAWXHI_erD7sGotYTc75jYea72RRhCrLdauO_KDRl-6YMeql4rft9m1GHxQ"
 headers = {
     "authorization": f"Bearer {access_token}"
 }
-routeList=["234","南環幹線","205","618","民權幹線"]
+routeList=["542"]#"234","南環幹線","205","618","民權幹線"]
 
 def get_data(url):
   response = requests.get(url, headers=headers)
@@ -20,21 +20,21 @@ for route in routeList:
     map_data = []
     stops_url=f"https://tdx.transportdata.tw/api/basic/v2/Bus/StopOfRoute/City/Taipei/{route}?&%24format=JSON"
     data=get_data(stops_url)
-    # if not data:
-    #     continue
+    if not data:
+        continue
     for subRoute in data:
         route_info = {
-            "route": subRoute["RouteName"]["Zh_tw"],
-            "direction": subRoute["Direction"],
-            "stops": []
+            "SubRoute": subRoute["SubRouteName"]["Zh_tw"],
+            "Direction": subRoute["Direction"],
+            "Stops": []
         }
 
         for stop in subRoute["Stops"]:
-            route_info["stops"].append({
-                "name": stop["StopName"]["Zh_tw"],
-                "lat": stop["StopPosition"]["PositionLat"],
-                "lon": stop["StopPosition"]["PositionLon"],
-                "sequence": stop["StopSequence"]
+            route_info["Stops"].append({
+                "Name": stop["StopName"]["Zh_tw"],
+                "Lat": stop["StopPosition"]["PositionLat"],
+                "Lon": stop["StopPosition"]["PositionLon"],
+                "Sequence": stop["StopSequence"]
             })
 
         map_data.append(route_info)
